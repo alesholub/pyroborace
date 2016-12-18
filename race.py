506ef9c36d3cdb776593498befdeeab05a028c83
alesholub <aholub@centrum.cz>
2016-12-18 17:29:01 +0100

    Usable racing version for most tracks

--- race.py
+++ race.py
@@ -11,6 +11,7 @@
 # - cut turns by predicted position
 
 # changelog:
+# v0.03 - 2016-12-18 - safer exits from turns and better speed regulation
 # v0.02 - 2016-12-16 - improved speed in turns but unsafe on exits
 # v0.01 - 2016-12-15 - basic universal racing version (for any track)
 
@@ -41,7 +42,7 @@ def segment_turn(segment):
 
 
 def drive(track):
-    print "race.py v0.02 (2016-12-16)"
+    print "race.py v0.03 (2016-12-18)"
     #time.sleep(10) # wait for game start
     soc = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     port = 4001
@@ -55,10 +56,13 @@ def drive(track):
     old_segment = None
     predicted_segment = old_segment
     min_speed = 14
-    max_speed = 109
-    target_speed = max_speed  
+    max_speed = 66
+    target_speed = max_speed
+    turn_speed = min_speed  
+    prev_turn_speed = min_speed  
     sum_e = 0
     last_speed = 0
+    mem_speed = {}
     start = 1
     while True:
         data = struct.pack('fffiBB', turn, gas, brake, 1, 11, ctr & 0xFF)
@@ -75,8 +79,10 @@ def drive(track):
 
             absVelX, absVelY = struct.unpack_from('ff', status, 80)
             speed = math.sqrt(absVelX*absVelX + absVelY*absVelY)
+
+            rpm,elevel = struct.unpack_from('ff', status, 16)
             
-            prediction_time = speed / 25  # sec
+            prediction_time = speed / 18  # sec
             absPosX1 = absPosX + prediction_time * absVelX
             absPosY1 = absPosY + prediction_time * absVelY
             predicted_segment, rel_pose1 = track.nearest_segment((absPosX1, absPosY1, heading))
@@ -103,15 +109,21 @@ def drive(track):
                             # straight
                             target_speed = max_speed
                             if predicted_segment.length<70:
-                                target_speed = max_speed * 0.55
+                                target_speed = max_speed * 0.85
                         else:
                             # turn
-                            target_speed = min_speed + predicted_segment.radius / 3
-                            if target_speed>50:
-                                target_speed = 50
+                            target_speed = min_speed + predicted_segment.radius / 2.8
+                            if target_speed>32:
+                                target_speed = 32 + predicted_segment.radius / 20
+                            if target_speed>49:
+                                target_speed = 49
                             if predicted_segment.radius>210:
-                                target_speed = min_speed + predicted_segment.radius / 5
+                                target_speed = 44 + predicted_segment.radius / 30
+                            if predicted_segment.radius>1000:
+                                target_speed = 48 + predicted_segment.radius / 100
+                            turn_speed = target_speed
                         old_segment = predicted_segment
+                        mem_speed[predicted_segment.name] = target_speed
                     else:
                         target_speed = target_speed  
                 else:
@@ -121,15 +133,20 @@ def drive(track):
                 if old_segment is not None and old_segment==segment and segment.radius is None:
                     # straight
                     target_speed = max_speed
-                    if segment.length<70:
-                        target_speed = max_speed * 0.55
-                if start==1: target_speed = 79
+                    if segment.name in mem_speed: target_speed = mem_speed[segment.name]
+                if segment.radius is not None:
+                    # turn
+                    target_speed = turn_speed
+                    if segment.name in mem_speed: target_speed = mem_speed[segment.name]
+                if start==1: target_speed = max_speed
+                
+                if target_speed>max_speed: target_speed = max_speed 
 
                 # PID speed regulation
-                P = 0.007
-                I = 0.006
-                D = 0.009
-                max_sum_e = 89
+                P = 0.05
+                I = 0.002
+                D = 2.0
+                max_sum_e = 1/I
                 e = target_speed - speed
                 sum_e += e
                 if sum_e>max_sum_e: sum_e = max_sum_e
@@ -138,13 +155,17 @@ def drive(track):
                 last_speed = speed
                 brake = 0.0
                 if gas>1.0: gas = 1
-                if gas<0.2 and speed>20:
+                if gas<0.0: gas = 0
+                if gas<0.3 and speed>min_speed:
                     gas = 0.01
                     if speed>(target_speed + 2):
-                        brake = 0.16
+                        brake = 0.19
+                        #brake = 0.2 + speed/300
+                if rpm>14900:
+                    gas = 0.01
 
                 dead_band = 0.1
-                max_dist_turn_deg = 19
+                max_dist_turn_deg = 29
 
                 if signed_dist < -dead_band:
                     # turn left
@@ -155,7 +176,12 @@ def drive(track):
                     turn += max(-max_dist_turn_deg, dead_band - signed_dist)
 
             if prev_segment != segment:
-                print segment.name,round(target_speed),round(speed),round(100*gas)/100,segment.radius,segment.arc,segment.length
+                #print segment.name,round(target_speed),round(speed),round(100*gas)/100,rpm,segment.radius,segment.arc,segment.length
+                radius,arc,length = segment.radius,segment.arc,segment.length
+                if radius is not None: radius = round(radius)
+                if arc is not None: arc = round(arc)
+                if length is not None: length = round(length)
+                print '{0:10} {1:4} {2:4} {3:4} {4:7} {5:4} {6:4} {6:4}'.format(segment.name,round(target_speed),round(speed),round(100*gas)/100,round(rpm),radius,arc,length)
                 prev_segment = segment
 
         except socket.error:
